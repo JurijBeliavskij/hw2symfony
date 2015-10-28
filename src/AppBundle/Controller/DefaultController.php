5bd6ade53f84e33c354ab72f5989ef29e12fd93f
Jurij <Jurij Beliavskij>
2015-10-29 01:15:41 +0200

    changed default Controller to show temperature

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,9 +14,10 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        $BASE_URL = "http://query.yahooapis.com/v1/public/yql";
+        $json=file_get_contents('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D479616%20AND%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=');
+        // Convert JSON to PHP object
+        $phpObj =  json_decode($json);
+        return new Response('<html><body>Current temperature in Vilnius: '.$phpObj->query->results->channel->item->condition->temp.' C</body></html>');
     }
 }
